f09fa81437de87d031a501289be8be42866332e9
inffy <10782843+inffy@users.noreply.github.com>
2026-07-03 06:19:46 +0000

    goose-linux 1.41.0

--- Casks/goose-linux.rb
+++ Casks/goose-linux.rb
@@ -1,6 +1,6 @@
 cask "goose-linux" do
-  version "1.37.0"
-  sha256 "829341de94b1b86e568d33558d522d5ca547f930a5fb473747110641de7dff51"
+  version "1.41.0"
+  sha256 "85f3954171dc207b8e42b9b655a59b3e13a1817de9d02ccb5b3fa7a90168fa0a"
 
   url "https://github.com/block/goose/releases/download/v#{version}/Goose-#{version}-1.x86_64.rpm",
       verified: "github.com/block/goose/"
