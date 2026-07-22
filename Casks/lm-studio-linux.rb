70e2404e73c12c0fb1fc105b454469704dfd91dc
inffy <10782843+inffy@users.noreply.github.com>
2026-07-22 05:38:47 +0000

    lm-studio-linux 0.4.20-1

--- Casks/lm-studio-linux.rb
+++ Casks/lm-studio-linux.rb
@@ -1,6 +1,6 @@
 cask "lm-studio-linux" do
-  version "0.4.16-1"
-  sha256 "0cb07557b7529072a5273e820da1e01642718e9b5d18f2fd7b7df0ed95d1ddaf"
+  version "0.4.20-1"
+  sha256 "6e1c9e7973ae892eef934d706612c920c04b24165861108d5883258801c64aed"
 
   url "https://installers.lmstudio.ai/linux/x64/#{version}/LM-Studio-#{version}-x64.AppImage"
   name "LM Studio"
